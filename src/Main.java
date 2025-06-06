04c3e863d37b78ce33f4ab9cb723e22e4662baad
yamamotonatuki <241058`@std.hi-joho.ac.jp>
2025-06-06 12:21:55 +0900

    ちゃぷたー8

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,30 @@
 // ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
 public class Main {
     public static void main(String[] args) {
-        //TIP ハイライトされたテキストにキャレットがある状態で <shortcut actionId="ShowIntentionActions"/> を押すと
-        // IntelliJ IDEA によるその修正案を確認できます。
-        System.out.printf("Hello and welcome!");
+        //1.勇者を生成
+        Hero h = new Hero();
+        //2.フィールドに初期値をセット
+        h.name = "ミナト";
+        h.hp = 100;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/> を押してコードのデバッグを開始します。<icon src="AllIcons.Debugger.Db_set_breakpoint"/> ブレークポイントを 1 つ設定しましたが、
-            // <shortcut actionId="ToggleLineBreakpoint"/> を押すといつでも他のブレークポイントを追加できます。
-            System.out.println("i = " + i);
-        }
+        Matango m1 = new Matango();
+        m1.hp= 50;
+        m1.suffix = 'A';
+
+        Matango m2 = new Matango();
+        m2.hp = 48;
+        m2.suffix = 'B';
+
+        //冒険のはじまり
+        h.slip();
+        m1.run();
+        m2.run();
+        h.run();
+        System.out.println("勇者" + h.name + "を生み出しました!");
+        //3.勇者のメソッドを呼び出してゆく
+        h.sit(5);
+        h.slip();
+        h.sit(30);
+        h.run();
     }
 }
